45a01533e8be5d40e0a51db4bf8898640c10fb77
tangxiangan <tangxiangan>
2023-04-24 09:17:03 +0800

    init

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,12 +1,11 @@
 package com.example.demo;
 
-import com.example.demo.properties.PaddleProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
 
 @SpringBootApplication
-@EnableConfigurationProperties({PaddleProperties.class})
+
 public class DemoApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -1,9 +1,6 @@
 package com.example.demo.controller;
 
-import com.example.demo.constant.PromConstant;
-import com.example.demo.pojo.MonitorDataVO;
-import com.example.demo.pojo.PromDataInfo;
-import com.example.demo.utils.PromUtils;
+
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,25 +13,12 @@ import java.util.List;
 @RequestMapping("/test")
 public class TestController {
 
-    @GetMapping("/get")
-    public PromDataInfo pay() {
-        return PromUtils.getDateInfo("http://124.223.7.184:9090",PromConstant.PROCESS_DISK_COUNT);
-    }
+//    @GetMapping("/get")
+//    public PromDataInfo pay() {
+//        return PromUtils.getDateInfo("http://124.223.7.184:9090",PromConstant.PROCESS_DISK_COUNT);
+//    }
 
-    @GetMapping("/get111")
-    public PromDataInfo get111() {
-        return PromUtils.getDiskInfo("http://124.223.7.184:9090");
-    }
 
-    @GetMapping("/getRange")
-    public PromDataInfo getRange() {
-        return PromUtils.getRangeDateInfo("http://124.223.7.184:9090",PromConstant.PROCESS_CPU_COUNT,1);
-    }
-
-    @GetMapping("/getAllInfo")
-    public List<MonitorDataVO> getAllInfo() {
-        return PromUtils.getAllInfo();
-    }
 
 
 
